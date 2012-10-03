0a275df848bcbf5e7fe24519c73bfd06ae11a856
Alysha <alyshakwok@gmail.com>
2012-10-02 22:24:14 -0700

    Updated login page UI. Added in bootstrap css for UI styling.

--- GetFit/src/com/cs410/getfit/client/LoginView.java
+++ GetFit/src/com/cs410/getfit/client/LoginView.java
@@ -11,16 +11,19 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class LoginView extends Composite implements EntryPoint{
-	@UiField TextBox userBox;
+	@UiField TextBox emailBox;
 	@UiField PasswordTextBox pwdBox;
 	
 	@UiTemplate("Login.ui.xml") 
-	interface Binder extends UiBinder<VerticalPanel, LoginView> {}
+	interface Binder extends UiBinder<Widget, LoginView> {}
 	private static final Binder uiBinder = GWT.create(Binder.class);
+	
+	public LoginView() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
 	
 	@UiHandler("loginBtn")
 	void onLoginClicked(ClickEvent event) {
@@ -30,8 +33,8 @@ public class LoginView extends Composite implements EntryPoint{
 
 	@Override
 	public void onModuleLoad() {
-		VerticalPanel vp = uiBinder.createAndBindUi(this);
-		RootPanel.get().add(vp);
+		LoginView loginView = new LoginView();
+		RootPanel.get().add(loginView);
 	}
 
 }
