d9f43bbb29c1a1edf7358b16deabf8c25dab575a
Alysha <alyshakwok@gmail.com>
2012-10-29 19:01:34 -0700

    Design review docs. HTTP change to get requests working with tomcat.

--- GetFit/src/com/cs410/getfit/client/json/HTTPRequestBuilder.java
+++ GetFit/src/com/cs410/getfit/client/json/HTTPRequestBuilder.java
@@ -6,9 +6,10 @@ public class HTTPRequestBuilder {
 	
 	private final static String header = "Content-Type";
 	private final static String headerValue = "application/json";
+	private final static String warFileName = "/getfit";
 	
 	public static RequestBuilder getPostRequest(String url) {
-		RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, url);
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, warFileName + url);
 		builder.setHeader(header, headerValue);
 		return builder;
 	}
